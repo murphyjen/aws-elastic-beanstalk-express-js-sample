11e8c90dafb71baf2ee352c454c2fa3135974740
Jen <jen.murphy@vmlyr.com>
2020-12-11 16:56:03 -0600

    manual confirmation test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is changed.'));
+app.get('/', (req, res) => res.send('Manual confirmation required.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
